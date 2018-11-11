590c9528867aa7c01c336fbd112ca5591702f722
Thomas Lee <thomaslee9474@gmail.com>
2018-11-11 11:02:06 -0800

    implemented listogram.py but not passing all tests

--- source/dictogram.py
+++ source/dictogram.py
@@ -2,14 +2,15 @@
 
 from __future__ import division, print_function  # Python 2 and 3 compatibility
 
+# Creates a histogram in dictioanary form on a given corpus of word: frequency key value pairs. 
 class Dictogram(dict):
     """Dictogram is a histogram implemented as a subclass of the dict type."""
 
     def __init__(self, word_list=None):
         """Initialise this histogram as a new dict and count given words."""
         super(Dictogram, self).__init__() # Initialising an empty histogram into self by inhereting dict. 
-        self.types = 0  # Unique words
-        self.tokens = 0 # Total frequency of all words.
+        self.types = 0  # Unique words in histogram.
+        self.tokens = 0 # Total frequency of all words in histogram.
       
         if word_list is not None:
             for word in word_list:
@@ -19,11 +20,10 @@ class Dictogram(dict):
         """Increase frequency count of given word by given count amount."""
         if word in self:
             self[word] += count
-            self.tokens += count
         else:
             self[word] = count 
-            self.tokens += count 
             self.types += 1
+        self.tokens += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
@@ -31,8 +31,7 @@ class Dictogram(dict):
             return self[word]
         else:
             return 0
-
-
+        
 def print_histogram(word_list):
     print('word list: {}'.format(word_list))
     # Create a dictogram and display its contents
