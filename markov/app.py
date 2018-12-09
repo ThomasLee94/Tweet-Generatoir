a1149fcf8b122dbfecec82d5c10e6745a2a17c7b
Thomas Lee <thomaslee9474@gmail.com>
2018-12-08 23:04:15 -0800

    [update] app

--- markov/app.py
+++ markov/app.py
@@ -1,5 +1,9 @@
 from flask import Flask 
-from sample import read_file, histogram, random_word, main
+from markov_nth_order import Markov_nth_order
+from weighted_sample import histogram, random_word_histogram
+from get_text_from_corpus import read_file
+from markov_app import main
+
 app = Flask(__name__)
 
 @app.route("/")
