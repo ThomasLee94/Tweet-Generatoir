036c02a91d2b481d801c111366907696fc9ccd08
Thomas Lee <thomaslee9474@gmail.com>
2018-12-12 10:53:45 -0800

    [deploying] heroku

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 def main():
     sentence_str = markov_app.main()
     return render_template("index.html", sentence=sentence_str)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
     
     
--- markov_app.py
+++ markov_app.py
@@ -3,7 +3,7 @@ from weighted_sample import histogram, random_word_histogram
 from get_text_from_corpus import read_file
 
 def main():
-    word_list = read_file("./corpus/stan/stan-transcript-s1-s8-&-s10-s20-no-brackets.txt")
+    word_list = read_file("stan-transcript-s1-s8-&-s10-s20-no-brackets.txt")
     # word_list = "one fish two fish red fish blue fish".split()
     # word_list = ['hi', 'hi', 'hi', 'bye', 'bye', 'bye', 'bye', 'hello', 'there', 'is', 'tokens', 'and', 'words']
     list_len = len(word_list)
