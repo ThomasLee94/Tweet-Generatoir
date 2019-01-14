27a9cd175fd15042afa87b8deecfb402091f0941
Thomas Lee <thomaslee9474@gmail.com>
2019-01-13 23:42:06 -0800

    [update] twitter api

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import markov_app
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import sys
+import twitter
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def main():
     sentence_str = markov_app.main()
     return render_template("index.html", sentence=sentence_str)
+
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    status = request.form['quote']
+    twitter.tweet(status)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
--- twitter.py
+++ twitter.py
@@ -1,5 +1,6 @@
 import os
 import dotenv
+from os import environ
 from requests_oauthlib import OAuth1Session
 
 dotenv.load_dotenv('.env')
@@ -26,4 +27,8 @@ resp = session.post(url, { 'status': status })
 
 # Show the text from the response
 print(resp.text)
+
+def tweet(status):
+    resp = session.post(url, { 'status': status })
+    return resp.text
 
